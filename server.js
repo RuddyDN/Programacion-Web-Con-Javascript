e56a2822a5a4952537dec80d1679a2915850576b
RuddyDn <ruddya.456@gmail.com>
2022-11-25 17:08:50 -0400

    Nuevos Cambios

--- server.js
+++ server.js
@@ -8,6 +8,24 @@ server.use("/node_modules",express.static(path.resolve(__dirname,"node_modules")
 server.get("/", function(req,res){
     res.sendFile(path.resolve(__dirname,"vistas","index.html"))
 });
+server.get("/formulario",function(req,res){
+    res.sendFile(path.resolve(__dirname,"vistas","formulario.html"))
+})
+server.get("/curriculum",function(req,res){
+    res.sendFile(path.resolve(__dirname,"vistas","curriculum.html"))
+})
+server.get("/maqueta",function(req,res){
+    res.sendFile(path.resolve(__dirname,"vistas","maqueta.html"))
+})
+server.get("/for",function(req,res){
+    res.sendFile(path.resolve(__dirname,"vistas","for.html"))
+})
+server.get("/contacto",function(req,res){
+    res.sendFile(path.resolve(__dirname,"vistas","contacto.html"))
+})
+server.get("/prompt",function(req,res){
+    res.sendFile(path.resolve(__dirname,"vistas","prompt.html"))
+})
 
 server.listen(port);
 
