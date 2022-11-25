ab1e5cc8d180e55136dba5baa4e26ec3208c86b2
RuddyDn <ruddya.456@gmail.com>
2022-11-25 08:53:02 -0400

    Construccion del server

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const server = express();
 const path = require('path');
-const port = 80;
+const port = 8100;
 
+server.use("/assets",express.static(path.resolve(__dirname,"/assets")));
+server.get("/", function(req,res){
+    res.sendFile(path.resolve(__dirname,"vistas","index.html"))
+});
+
+server.listen(port);
+
+console.log(`Servidor cooriendo en: https://localhost:${port}.com`)
